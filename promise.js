db849e2bbb687d54a7825b675d28dcb8f7162061
rashedrk <rashedrk001@gmail.com>
2023-03-11 23:22:12 +0600

    promise create and consume

---
+++ promise.js
@@ -0,0 +1,11 @@
+const getData = new Promise((resolve, reject) => {
+    const num = Math.random()*10;
+    if (num < 5) {
+        resolve(1234456)
+    } else {
+        reject('No data available')
+    }
+})
+getData
+    .then(data => console.log(data))
+    .catch(err => console.log('Error:' + err))
